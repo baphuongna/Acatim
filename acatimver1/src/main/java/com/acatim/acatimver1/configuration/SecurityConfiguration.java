372ebd51c8bf9d16725fab553224f97dc92e1bd4
baphuongna <30903783+baphuongna@users.noreply.github.com>
2019-02-12 23:18:34 +0700

    fix lại 1 xíu

--- acatimver1/src/main/java/com/acatim/acatimver1/configuration/SecurityConfiguration.java
+++ acatimver1/src/main/java/com/acatim/acatimver1/configuration/SecurityConfiguration.java
@@ -46,11 +46,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         http.
                 authorizeRequests()
                 .antMatchers("/").permitAll()
-                .antMatchers("/login").permitAll()
+                .antMatchers("/index").permitAll()
                 .antMatchers("/registration").permitAll()
                 .antMatchers("/admin/**").hasAuthority("ADMIN").anyRequest()
                 .authenticated().and().csrf().disable().formLogin()
-                .loginPage("/login").failureUrl("/login?error=true")
+                .loginPage("/index").failureUrl("/index?error=true")
                 .defaultSuccessUrl("/admin/home")
                 .usernameParameter("email")
                 .passwordParameter("password")
