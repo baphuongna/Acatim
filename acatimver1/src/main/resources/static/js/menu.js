3d5fd3465de89201cdc5ad238d270eab743d514a
MSI GL63 <MSI GL63@MSI>
2019-03-20 12:38:21 +0700

    Nguyen Viet Duy - Fix Menu and add new filter

--- acatimver1/src/main/resources/static/js/menu.js
+++ acatimver1/src/main/resources/static/js/menu.js
@@ -13,3 +13,14 @@ $(document).ready(function(){
         }
     );
 });
+
+$(document).on("scroll", function(){
+	if
+  ($(document).scrollTop() > 86){
+	  $("#banner").addClass("shrink");
+	}
+	else
+	{
+		$("#banner").removeClass("shrink");
+	}
+});
