84eb479d9cc62074c099e902854af1eb95416868
baphuongna <30903783+baphuongna@users.noreply.github.com>
2019-04-05 00:57:49 +0700

    phuong - fix

--- acatimver1/src/main/java/com/acatim/acatimver1/Acatimver1Application.java
+++ acatimver1/src/main/java/com/acatim/acatimver1/Acatimver1Application.java
@@ -2,13 +2,12 @@ package com.acatim.acatimver1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@EntityScan(basePackages = "com.acatim.acatimver1.model")
 @SpringBootApplication
 public class Acatimver1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Acatimver1Application.class, args);
 	}
+
 }
