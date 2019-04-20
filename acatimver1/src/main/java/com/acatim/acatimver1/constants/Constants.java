56f2f32998c7bfcb5149407c570ad15923e38088
phong <phong@DESKTOP-131KB8G>
2019-04-21 00:37:11 +0700

    update

--- acatimver1/src/main/java/com/acatim/acatimver1/constants/Constants.java
+++ acatimver1/src/main/java/com/acatim/acatimver1/constants/Constants.java
@@ -6,10 +6,10 @@ public class Constants {
 	public static final String index_header_text = "Bạn không biết nên học ở đâu? bạn không tìm được một giáo\r\n" + 
 			"						viên giỏi? bạn khó khăn trong việc tiếp thu kiến thức? Hãy để\r\n" + 
 			"						chúng tôi giúp bạn!"; 
-	public static final String index_goal_description = "Chúng tôi giúp bạn giải đáp các câu\r\n" + 
-			"						hỏi như: tìm gia sư ở đâu? học trung tâm nào tốt? giá cả như thế\r\n" + 
-			"						nào? có đội ngũ chăm sóc bạn 24/24, giúp bạn thăng tiến trên con\r\n" + 
-			"						đường học tập phía trước!"; 
+	public static final String index_goal_description = "HỌC VIÊN, GIA SƯ  HAY TRUNG TÂM?";
+	public static final String index_goal_description2 = "Bạn là một học viên? Nhận ngay mã giảm giá khóa học tại các trung tâm giáo dục hàng đầu tại Việt Nam. Đội ngũ tư vấn, giải đáp tất cả các câu hỏi liên quan, dễ dàng tìm kiếm, đánh giá và lựa chọn những \"kho tàng kiến thức\" phù hợp.";		
+	public static final String index_goal_description3 = "Bạn là một giáo viên của chúng tôi? Lập tức được chia sẻ thông tin về chuyên môn giảng dạy, được giới thiệu những học viên có nhu cầu từ Hệ thống. Đăng tải các khóa học của riêng mình ngay tại trang web.";
+	public static final String index_goal_description4 = "Bạn có một Trung tâm dạy học? Có ngay cơ hội tuyển thêm học viên cho Trung tâm của bạn thông qua Hệ thống, công khai toàn bộ những khóa học chất lượng, nhận được những phản hồi, đánh giá trực quan từ học viên thông qua tính năng Phản hồi.";
 	//button
 	public static final String btn_course_detail = "xem chi tiết";
 	public static final String btn_tim_khoa_hoc = "Tìm kiếm khóa học";
