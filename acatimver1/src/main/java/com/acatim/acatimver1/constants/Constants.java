55339d75d811898161a75b519ff49e266a5076a6
phong <phong@DESKTOP-131KB8G>
2019-04-30 16:09:31 +0700

    fix + update

--- acatimver1/src/main/java/com/acatim/acatimver1/constants/Constants.java
+++ acatimver1/src/main/java/com/acatim/acatimver1/constants/Constants.java
@@ -6,6 +6,7 @@ public class Constants {
 	public static final String index_header_text = "Bạn không biết nên học ở đâu? bạn không tìm được một giáo\r\n" + 
 			"						viên giỏi? bạn khó khăn trong việc tiếp thu kiến thức? Hãy để\r\n" + 
 			"						chúng tôi giúp bạn!"; 
+	public static final String index_header_text_teacher_studyCenter = "Hãy để chúng tôi giúp bạn giới thiệu Khóa học của bạn đến với mọi người!";
 	public static final String index_goal_description = "HỌC VIÊN, GIA SƯ  HAY TRUNG TÂM?";
 	public static final String index_goal_description2 = "Bạn là một học viên? Nhận ngay mã giảm giá khóa học tại các trung tâm giáo dục hàng đầu tại Việt Nam. Đội ngũ tư vấn, giải đáp tất cả các câu hỏi liên quan, dễ dàng tìm kiếm, đánh giá và lựa chọn những \"kho tàng kiến thức\" phù hợp.";		
 	public static final String index_goal_description3 = "Bạn là một giáo viên của chúng tôi? Lập tức được chia sẻ thông tin về chuyên môn giảng dạy, được giới thiệu những học viên có nhu cầu từ Hệ thống. Đăng tải các khóa học của riêng mình ngay tại trang web.";
@@ -13,6 +14,7 @@ public class Constants {
 	//button
 	public static final String btn_course_detail = "xem chi tiết";
 	public static final String btn_tim_khoa_hoc = "Tìm kiếm khóa học";
+	public static final String btn_them_khoa_hoc = "Thêm khóa học";
 	public static final String btn_remember_me = "Nhớ tài khoản ?";
 	public static final String btn_register = "Đăng Ký";
 	
