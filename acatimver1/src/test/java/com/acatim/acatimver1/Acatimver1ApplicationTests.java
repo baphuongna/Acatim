d163790b091f9dba2329abef7d6cbf6f667d4702
baletran <46474673+baletran@users.noreply.github.com>
2019-05-05 16:30:19 +0700

    unit test

--- acatimver1/src/test/java/com/acatim/acatimver1/Acatimver1ApplicationTests.java
+++ acatimver1/src/test/java/com/acatim/acatimver1/Acatimver1ApplicationTests.java
@@ -1,17 +1,54 @@
 package com.acatim.acatimver1;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+
+import org.springframework.transaction.annotation.Transactional;
+import com.acatim.acatimver1.dao.StudentDAO;
+import com.acatim.acatimver1.dao.StudyCenterDAO;
+import com.acatim.acatimver1.dao.TeacherDAO;
+import com.acatim.acatimver1.entity.Student;
+import com.acatim.acatimver1.entity.StudyCenter;
+import com.acatim.acatimver1.entity.Teacher;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class Acatimver1ApplicationTests {
 
+	@Autowired
+	private TeacherDAO teacherDAO;
+
 	@Test
-	public void contextLoads() {
+	@Transactional
+	@Rollback(true)
+	public void testAddTeacher() {
+		Teacher teacherAdd = new Teacher();
+		teacherAdd.setUserName("Quangtran");
+		teacherAdd.setGender(true);
+		teacherAdd.setDescription("Có tâm với việc dạy học");
+		teacherDAO.addTeacherInfo(teacherAdd);
+		Teacher teacher = teacherDAO.getTeacherByAccount("Quangtran");
+		Assert.assertEquals(teacherAdd.getUserName(), teacher.getUserName());
 	}
 
+	@Autowired
+	private StudentDAO studentDAO;
+	@Test
+	@Transactional
+	@Rollback(true)
+	public void testAddStudent() {
+		Student studentAdd = new Student();
+		studentAdd.setUserName("Quangtran");
+		studentAdd.setGender(true);
+		studentDAO.addStudentInfo(studentAdd);
+		Student student = studentDAO.findInfoUserAccount("Quangtran");
+		Assert.assertEquals(studentAdd.getUserName(), student.getUserName());
+	}
+	
 }
-
